66fce304f6b0bab50a3c152be8dfef1a2e056f83
sarwar-jahan1 <sarwarjahans2001@gamil.com>
2026-03-03 00:20:05 +0600

    Responsive and added some logical portion

---
+++ script/index.js
@@ -0,0 +1,50 @@
+const loadLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+        .then((res) => res.json())
+        .then((json) => displayLesson(json.data));
+};
+const loadLevelWord=(id)=>{
+    const url=`https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLevelWord(data.data))
+}
+
+const displayLevelWord=(words)=>{
+    const wordContainer=document.getElementById('word-container');
+    wordContainer.innerHTML="";
+
+    words.forEach(word=>{
+        // console.log(word);
+        const card=document.createElement("div");
+        card.innerHTML=`
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="text-2xl font-bold">${word.word}</h2>
+            <p class="font-serif">Meaning /Pronounciation</p>
+            <div class="font-bangla text-2xl font-medium">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+        `
+        wordContainer.appendChild(card);
+    })
+
+}
+const displayLesson=(lessons)=>{
+    const levelContainer=document.getElementById("level-container");
+    levelContainer.innerHTML="";
+
+    for(let lesson of lessons){
+        const btnDiv=document.createElement("div");
+        btnDiv.innerHTML=`
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+        <i class="fa-solid fa-book-open"></i>Lesson -${lesson.level_no}
+        </button>
+        `;
+        levelContainer.appendChild(btnDiv);
+    }
+}
+loadLessons();
+
