25fbbe4fed6d431c8bb3f1fd2f892ffd6d8c5a07
sarwar-jahan1 <sarwarjahans2001@gamil.com>
2026-03-04 00:31:06 +0600

    Final push for this project inshaAllah

--- script/index.js
+++ script/index.js
@@ -1,36 +1,110 @@
+const createElements=(arr)=>{
+    const htmlElements=arr.map((el)=> `<span class="btn">${el} </span>`);
+    return htmlElements.join(" ");
+};
+
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+const manageSpinner=(status)=>{
+    if(status==true){
+        document.getElementById('spinner').classList.remove('hidden');
+        document.getElementById('word-container').classList.add('hidden');
+    }else{
+        document.getElementById('spinner').classList.add('hidden');
+        document.getElementById('word-container').classList.remove('hidden');
+    }
+}
+
 const loadLessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all")
         .then((res) => res.json())
         .then((json) => displayLesson(json.data));
 };
+
+const removeActive=()=>{
+    const lessonButtons=document.querySelectorAll('.lesson-btn');
+    lessonButtons.forEach(btn=>btn.classList.remove('active'));
+}
 const loadLevelWord=(id)=>{
+    manageSpinner(true);
     const url=`https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => displayLevelWord(data.data))
+    
+    .then((data => {
+        removeActive();
+        const clickBtn=document.getElementById(`lesson-btn-${id}`)
+        clickBtn.classList.add('active');
+        displayLevelWord(data.data);
+    }))
 }
+
+const loadWordDetail = async(id)=>{
+    const url=`https://openapi.programming-hero.com/api/word/${id}`
+    const res=await fetch(url);
+    const details=await res.json();
+    displayWordDetails(details.data);
+};
+const displayWordDetails=(word)=>{
+    console.log(word);
+    const detailsBox=document.getElementById('details-container');
+    detailsBox.innerHTML=`
+    <div class="">
+                    <h2 class="text-2xl font-bold">${word.word} ( <i class="fa-solid fa-microphone-lines"></i> :${word.pronunciation})</h2>
+                </div>
+
+                <div>
+                    <h2 class=" font-bold">Meaning</h2>
+                    <p class="font-bangla">${word.meaning}</p>
+                </div>
+                <div>
+                    <h2 class=" font-bold">Example</h2>
+                    <p class="">${word.sentence}</p>
+                </div>
+                <div class="">
+                    <h2 class=" font-bold">Synonym</h2>
+                    <div>${createElements(word.synonyms)}</div>
+                </div>
+                `;
+    document.getElementById("word_modal").showModal();
+};
 
 const displayLevelWord=(words)=>{
     const wordContainer=document.getElementById('word-container');
     wordContainer.innerHTML="";
+
+    if(words == 0){
+        wordContainer.innerHTML=`
+         <div class="font-bangla text-center col-span-full rounded-xl py-10 space-y-6">
+            <img class="mx-auto" src="./assets/alert-error.png"/>
+            <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+        </div>
+        `;
+        manageSpinner(false)
+        return;
+    }
 
     words.forEach(word=>{
         // console.log(word);
         const card=document.createElement("div");
         card.innerHTML=`
         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="text-2xl font-bold">${word.word}</h2>
+            <h2 class="text-2xl font-bold">${word.word ? word.word: "Invalid Word"}</h2>
             <p class="font-serif">Meaning /Pronounciation</p>
-            <div class="font-bangla text-2xl font-medium">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="font-bangla text-2xl font-medium">"${word.meaning ? word.meaning:"never find"} / ${word.pronunciation ? word.pronunciation : "pronunciation pawa jaini"}"</div>
             <div class="flex justify-between items-center">
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-circle-info"></i></button>
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-volume-high"></i></button>
+                <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-circle-info"></i></button>
+                <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>
         `
         wordContainer.appendChild(card);
     })
-
+    manageSpinner(false);
 }
 const displayLesson=(lessons)=>{
     const levelContainer=document.getElementById("level-container");
@@ -39,7 +113,7 @@ const displayLesson=(lessons)=>{
     for(let lesson of lessons){
         const btnDiv=document.createElement("div");
         btnDiv.innerHTML=`
-        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+        <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn">
         <i class="fa-solid fa-book-open"></i>Lesson -${lesson.level_no}
         </button>
         `;
@@ -48,3 +122,18 @@ const displayLesson=(lessons)=>{
 }
 loadLessons();
 
+document.getElementById('btn-search').addEventListener('click' , ()=>{
+    removeActive()
+    const input=document.getElementById('input-search');
+    const searchValue=input.value.trim().toLowerCase();
+    console.log(searchValue);
+
+    fetch('https://openapi.programming-hero.com/api/words/all')
+    .then(res=> res.json())
+    .then(data=> {
+        const allWords=data.data;
+        console.log(allWords);
+        const filterWords=allWords.filter(word=>word.word.toLowerCase().includes(searchValue));
+        displayLevelWord(filterWords);
+    })
+})
